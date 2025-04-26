797d6158c9ac1a600d7b6a2ae202a1309c197a56
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2025-04-26 02:15:23 -0300

    Lista de usuários via Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/users/user.interface';
+import { UsersList } from './data/users-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  usersList: IUser[] = [];
   userSelected: IUser = {} as IUser;
   showUserDetails: boolean = false;
 
   onUserSelected(user: IUser){
     this.userSelected = user;
     this.showUserDetails = true;
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.usersList = UsersList;
+    }, 3000); 
   }
 }
--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { UsersList } from 'src/app/data/users-list';
 import { IUser } from 'src/app/interfaces/users/user.interface';
 
@@ -9,7 +9,7 @@ import { IUser } from 'src/app/interfaces/users/user.interface';
 })
 export class UsersListComponent {
   displayedColumns: string[] = ['name', 'date', 'status'];
-  usersList: IUser[] = UsersList;
+  @Input({ required: true }) usersList: IUser[] = [];
 
   @Output('userSelected') userSelectedEmit = new EventEmitter<IUser>();
 
