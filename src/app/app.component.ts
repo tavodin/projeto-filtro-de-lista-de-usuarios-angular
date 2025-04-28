6d24dd5b91491001cc0458b6f094fdc1de1e95ff
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2025-04-28 14:20:56 -0300

    Evento do Botão de Filtrar, Lógica do Filtro por Nome e Status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/users/user.interface';
 import { UsersList } from './data/users-list';
+import { IFilterOptions } from './interfaces/filter-options.interface';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ import { UsersList } from './data/users-list';
 })
 export class AppComponent implements OnInit {
   usersList: IUser[] = [];
+  usersListFiltered: IUser[] = [];
   userSelected: IUser = {} as IUser;
   showUserDetails: boolean = false;
 
@@ -20,6 +22,48 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     setTimeout(() => {
       this.usersList = UsersList;
+      this.usersListFiltered = this.usersList;
     }, 1500); 
   }
+
+  onFilter(filterOptions : IFilterOptions) {
+    console.log(filterOptions);
+    this.usersListFiltered = this.filtersUserList(filterOptions, this.usersList);
+  }
+
+  filtersUserList(filterOptions: IFilterOptions, usersList: IUser[]): IUser[] {
+    let filteredList: IUser[] = [];
+
+    filteredList = this.filterUsersListByName(filterOptions.name, usersList);
+    filteredList = this.filterUsersListByStatus(filterOptions.status, filteredList);
+
+    return filteredList;
+  }
+
+  filterUsersListByStatus(status: boolean | undefined, usersList: IUser[]): IUser[] {
+    const STATUS_IS_UNDEFINED = status === undefined;
+
+    if(STATUS_IS_UNDEFINED) {
+      return usersList;
+    }
+
+    const filteredList = usersList.filter((user) => user.ativo === status);
+
+    return filteredList;
+  }  
+
+  filterUsersListByName(name: string, usersList: IUser[]): IUser[] {
+    const NAME_NOT_TYPED = name === undefined;
+
+    if(NAME_NOT_TYPED) {
+      return usersList;
+    }
+
+    const filteredList = usersList.filter((user) => 
+      user.nome.toLowerCase().includes(name.toLocaleLowerCase())
+    );
+
+    return filteredList;
+  }
+
 }
